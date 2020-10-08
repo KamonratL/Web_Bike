c35b58cf2f317e7bf1cf3763b9a8796cf0384975
KamonratL <46518277+KamonratL@users.noreply.github.com>
2020-10-08 23:23:42 +0700

    Add files upload
    Add files upload PHP login and register

---
+++ index.php
@@ -0,0 +1,49 @@
+<!-- check if not register return login -->
+<?php
+    session_start();
+
+    if (!isset($_SESSION['username'])) {
+        $_SESSION['msg'] = "You must log in first";
+        header('location: login.php');
+    }
+
+    if (isset($_GET['logout'])) {
+        session_destroy();
+        unset($_SESSION['username']);
+        header('location: login.php');
+    }
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="style.css">
+        <title>Home Page</title>
+    </head>
+    <body>
+        <div class="homeheader">
+            <h2>Home Page</h2>
+        </div>
+
+        <div class="homecontent">
+            <!-- notification message -->
+            <?php if (isset($_SESSION['success'])) : ?>
+                <div class="success">
+                    <h3>
+                        <?php 
+                            echo $_SESSION['success'];
+                            unset($_SESSION['success']);
+                        ?>
+                    </h3>
+                </div>
+            <?php endif ?>
+            
+            <!-- logged in user inforamtion -->
+            <?php if (isset($_SESSION['username'])) : ?>
+                <p>Welcome <strong><?php echo $_SESSION['username']; ?></strong></p>
+                <p><a href="index.php?logout='1'" style="color: red;">Logout</a></p>
+            <?php endif ?>
+        </div>
+
+    </body>
+</html>
